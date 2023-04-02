dda61f81f0e10244be0d43e97c7c16b405365018
Yogendra Manawat <yogendramanawat@gmail.com>
2023-04-02 22:36:54 +0530

    logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import TagSelector from "./Components/TagSelector";
+import logo from "./Images/Logo.png";
 
 function App() {
   const [artMedium, setArtMedium] = useState("");
@@ -11,59 +12,72 @@ function App() {
   const [artist, setArtist] = useState("");
   const [inputValue, setInputValue] = useState("");
 
-  const combinedString = `${artMedium}, ${subject}, ${action}, ${attire}, ${background}, ${artist}, ${inputValue}`;
+  const combinedString = `${artMedium} ${subject} ${action} ${attire} ${background} ${artist} ${inputValue}`;
 
   return (
-    <div className="main">
-      <input
-        type="text"
-        className="InputField"
-        value={combinedString === ", , , , , , " ? "" : `${combinedString}${inputValue}`}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="Your Simplified Prompt Here"
-      />
-      <div className="container">
-        <TagSelector
-          label="Art Medium"
-          tags={["Illustration", "Photo", "Digital Art", "Painting"]}
-          onTagSelect={() => {}}
-          onChange={setArtMedium}
-        />
-        <TagSelector
-          label="Subject"
-          tags={["Person", "Men", "Women", "Dog"]}
-          onTagSelect={() => {}}
-          onChange={setSubject}
-        />
-        <TagSelector
-          label="Action"
-          tags={["Standing", "Running", "Fighting", "Dancing"]}
-          onTagSelect={() => {}}
-          onChange={setAction}
-        />
-        <TagSelector
-          label="Attire"
-          tags={["Casual", "Saree", "Hoodie", "Suit"]}
-          onTagSelect={() => {}}
-          onChange={setAttire}
-        />
-        <TagSelector
-          label="Background"
-          tags={["Sunny", "Cloudy", "Rainy", "Hilly"]}
-          onTagSelect={() => {}}
-          onChange={setBackground}
-        />
-        <TagSelector
-          label="Artist"
-          tags={["Greg rutkowski", "Ross draws", "Ghibhli Studios", "Ily Kuvshinov"]}
-          onTagSelect={() => {}}
-          onChange={setArtist}
-        />
+    <>
+      <div style={{ margin: "32px" }}>
+        <img src={logo} />
       </div>
-      <button className="generate-button">
-        <span>Generate Image</span>
-      </button>
-    </div>
+
+      <div className="main">
+        <input
+          type="text"
+          className="InputField"
+          value={
+            combinedString === "      " ? "" : `${combinedString}${inputValue}`
+          }
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Your Simplified Prompt Here"
+        />
+        <div className="container">
+          <TagSelector
+            label="Art Medium"
+            tags={["Illustration", "Photo", "Digital Art", "Painting"]}
+            onTagSelect={() => {}}
+            onChange={setArtMedium}
+          />
+          <TagSelector
+            label="Subject"
+            tags={["Person", "Men", "Women", "Dog"]}
+            onTagSelect={() => {}}
+            onChange={setSubject}
+          />
+          <TagSelector
+            label="Action"
+            tags={["Standing", "Running", "Fighting", "Dancing"]}
+            onTagSelect={() => {}}
+            onChange={setAction}
+          />
+          <TagSelector
+            label="Attire"
+            tags={["Casual", "Saree", "Hoodie", "Suit"]}
+            onTagSelect={() => {}}
+            onChange={setAttire}
+          />
+          <TagSelector
+            label="Background"
+            tags={["Sunny", "Cloudy", "Rainy", "Hilly"]}
+            onTagSelect={() => {}}
+            onChange={setBackground}
+          />
+          <TagSelector
+            label="Artist"
+            tags={[
+              "Greg rutkowski",
+              "Ross draws",
+              "Ghibhli Studios",
+              "Ily Kuvshinov",
+            ]}
+            onTagSelect={() => {}}
+            onChange={setArtist}
+          />
+        </div>
+        <button className="generate-button">
+          <span>Generate Image</span>
+        </button>
+      </div>
+    </>
   );
 }
 
